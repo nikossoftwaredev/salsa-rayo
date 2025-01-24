72dde58c1b981e7c539e4c2d7cdef57106705449
nikos <nikos@zap.xyz>
2025-01-24 12:13:27 +0200

    TEST

---
+++ app/tg-channels/[telegramChannelId]/page.tsx
@@ -0,0 +1,54 @@
+import { Metadata } from "next";
+type Props = {
+  params: Promise<{ telegramChannelId: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const telegramChannelId = (await params).telegramChannelId;
+
+  const tgChannel = {
+    username: "zap",
+    title: "Zap",
+  };
+
+  const title = `Zap - TG Channel - ${tgChannel?.username}`;
+  const description = `View ${tgChannel?.title} on Zap ⚡`;
+  const ogImageUrl = `https://zap.xyz/api/og?tg-channel=${encodeURIComponent(
+    telegramChannelId
+  )}`;
+
+  const ogImage = {
+    url: ogImageUrl,
+    secureUrl: ogImageUrl,
+    width: 1200,
+    height: 630,
+    alt: `Dynamic ${title} Image`,
+  };
+
+  return {
+    title,
+    description,
+    openGraph: {
+      type: "website",
+      title,
+      description,
+      images: [ogImage],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      creator: "@zap",
+      images: [ogImage],
+    },
+  };
+}
+
+export default async function Home() {
+  return (
+    <div className="flex w-full items-center justify-center px-6 py-9">
+      HELLO
+    </div>
+  );
+}
