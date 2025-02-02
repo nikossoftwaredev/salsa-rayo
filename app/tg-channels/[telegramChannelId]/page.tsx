ec7e3f45ccb1c9d4c2aadbaa9290827198ae8fdf
nikos-dim <nikos@zap.xyz>
2025-02-02 18:59:40 +0200

    aaa

--- app/tg-channels/[telegramChannelId]/page.tsx
+++ app/tg-channels/[telegramChannelId]/page.tsx
@@ -15,7 +15,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 
   const title = `Zap - TG Channel - ${tgChannel?.username}`;
-  const description = `View ${tgChannel?.title} on Zap ⚡ 2222`;
+  const description = `View ${tgChannel?.title} on Zap ⚡ 3333`;
   const ts = Date.now();
   const ogImageUrl = `https://salsa-rayo.vercel.app/api/og?tg-channel=${encodeURIComponent(
     telegramChannelId
